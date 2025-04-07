0a21641e2b21628ae9e4b0cf667f7839f75b9d0e
JuDyas <gden886@gmail.com>
2025-04-07 20:53:35 +0200

    implementation ÑСCountWords for handle requests

--- internal/handlers/text.go
+++ internal/handlers/text.go
@@ -1,1 +1,26 @@
-package handlers
+package handler
+
+import (
+	"fmt"
+	"github.com/JuDyas/JenkinsTry-3/internal/service"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type WordCountRequest struct {
+	Text string `json:"text"`
+}
+
+func CountWords(c echo.Context) error {
+	var req WordCountRequest
+	fmt.Println(req)
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "Invalid input"})
+	}
+
+	svc := service.NewWordCountService()
+	wordCount := svc.CountWords(req.Text)
+
+	return c.JSON(http.StatusOK, map[string]int{"word_count": wordCount})
+}
