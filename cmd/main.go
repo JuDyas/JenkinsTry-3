fb463909b733ee1b8f0513a3cf13c1940691691f
JuDyas <gden886@gmail.com>
2025-04-07 20:47:59 +0200

    add main server with path /wordcount

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	handler "github.com/JuDyas/JenkinsTry-3/internal/handlers"
+	"log"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.POST("/wordcount", handler.CountWords)
+	log.Fatal(e.Start(":8080"))
+}
