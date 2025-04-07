29300cbd0f5ae566eaa8f05ad2281a1645aed9f5
JuDyas <gden886@gmail.com>
2025-04-07 21:55:53 +0200

    add func word-cont

--- internal/service/word_count.go
+++ internal/service/word_count.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"regexp"
+	"strings"
+)
+
+type WordCountService struct{}
+
+func NewWordCountService() *WordCountService {
+	return &WordCountService{}
+}
+
+func (svc *WordCountService) CountWords(text string) int {
+	text = strings.ToLower(strings.ReplaceAll(text, "\n", " "))
+
+	re := regexp.MustCompile(`[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*`)
+	words := re.FindAllString(text, -1)
+
+	return len(words)
+}
